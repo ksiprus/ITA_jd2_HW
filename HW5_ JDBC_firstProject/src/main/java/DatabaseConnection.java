96aaa25592a9668c2b1cfb3f25fde20fa35e9688
Uladzimir Shunevich <ksiprus@mail.ru>
2024-11-12 12:39:46 +0300

    исправил класс DatabaseConnection согласно замечания

--- HW5_ JDBC_firstProject/src/main/java/DatabaseConnection.java
+++ HW5_ JDBC_firstProject/src/main/java/DatabaseConnection.java
@@ -1,9 +1,12 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/OnlyMyShems";
-    private static final String USER = "root";
-    private static final String PASSWORD = "12345678";
+    private static final String URL = "URL";
+    private static final String USER = "USER";
+    private static final String PASSWORD = "PASSWORD";
+    List<String> results = new ArrayList<>();
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
@@ -16,7 +19,7 @@ public class DatabaseConnection {
             // Добавление нового сотрудника
             manager.addEmployee("Иван", "Иванов", "ivanov@example.com", "IT", 3000.00, Date.valueOf("2024-11-10"));
             manager.addEmployee("Владимир", "Шуневич", "ksiprus@mail.ru", "sales", 2500.50, Date.valueOf("2024-11-21"));
-            manager.addEmployee("Петр","Петров","legasy87@gmail.com","administration",1500.00, Date.valueOf("2024-11-22"));
+            manager.addEmployee("Петр", "Петров", "legasy87@gmail.com", "administration", 1500.00, Date.valueOf("2024-11-22"));
             // Изменение данных сотрудника
             manager.updateEmployee(15, "Иван", "Иванович", "ivanov@example.com", "IT", 5000.00, Date.valueOf("2024-11-10"));
 
